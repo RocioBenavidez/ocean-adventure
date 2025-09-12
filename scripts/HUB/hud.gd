ae671d3a7c5a4c21a457d05e07732f37e21b055c
kumamania <166665261+kumamania@users.noreply.github.com>
2025-09-12 20:52:33 -0300

    Add: sistema de puntos

--- scripts/HUB/hud.gd
+++ scripts/HUB/hud.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 @onready var health_bar: TextureProgressBar = $HealthBar
 @onready var score_label = $ScoreLabel
 
+
 func set_health(value: int):
 	health_bar.value = value
 
@@ -16,8 +17,30 @@ func _ready():
 		var player = players[0]  # en tu juego seguramente haya solo 1
 		player.connect("health_changed", Callable(self, "_on_player_health_changed"))
 		set_health(player.vida)  # inicializar la barra
+		
 	else:
 		print("⚠️ No se encontró ningún nodo en el grupo 'player'")
 		
+	var timer_node = get_node("/root/HUB/timerGame")  # Asegúrate de tener la ruta correcta
+	if timer_node:
+		timer_node.connect("timer_updated", Callable(self, "_on_timer_updated"))
+		
+	else:
+		print("⚠️ No se encontró el nodo de cronómetro.")
+		
+
+
 func _on_player_health_changed(value):
 	set_health(value)
+
+func _on_timer_updated(minutes, seconds, msec, puntos):
+	# Actualiza el texto del cronómetro
+	$Minutes2.text = "%02d" % minutes
+	$Seconds2.text = "%02d" % seconds
+	$Msecs2.text = "%03d" % msec
+	
+	# Actualiza los puntos
+
+
+func _on_timer_game_ready() -> void:
+	pass # Replace with function body.
--- scripts/HUB/timerGame.gd
+++ scripts/HUB/timerGame.gd
@@ -4,12 +4,20 @@ var time: float = 0.0
 var minutes: int = 0
 var seconds: int = 0
 var msec: int = 0
+var puntos: int = 0
+
+signal timer_updated(minutes, seconds, msec)
 
 func _process(delta) -> void:
 	time += delta
 	msec = fmod(time, 1) * 100
 	seconds = fmod(time, 60)
 	minutes = fmod(time, 3600) / 60
+	
+	if int(seconds) != int(fmod(time - delta, 60)):
+		puntos += 10  
+		emit_signal("timer_updated", minutes, seconds, msec, puntos)
+	
 	$Minutes.text = "%02d:" % minutes
 	$Seconds.text = "%02d:" % seconds
 	$Msecs.text = "%03d:" % msec
